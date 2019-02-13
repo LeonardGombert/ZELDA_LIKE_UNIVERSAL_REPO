8d6abe1f0bad7c4c15132984d6bd75cff807a108
SamuelD <workflow.samuel@gmail.com>
2019-02-13 10:13:26 +0100

    Left-Right Enemy
    Scene / Code for 1 axis enemy.

---
+++ AxisEnemy/Assets/_Scripts/PatrolEnemyAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolEnemyAI : MonoBehaviour
+{
+
+    public Transform[] patrolPoints;
+    public float speed;
+    Transform currentPatrolPoint;
+    int currentPatrolIndex;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPatrolIndex = 0;
+        currentPatrolPoint = patrolPoints[currentPatrolIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+        if (Vector2.Distance(transform.position, currentPatrolPoint.position) <.1f)
+        {
+            if(currentPatrolIndex +1 < patrolPoints.Length)
+            {
+                currentPatrolIndex++;
+            }
+            else
+            {
+                currentPatrolIndex = 0;
+            }
+            currentPatrolPoint = patrolPoints[currentPatrolIndex];
+        }
+        Vector2 patrolPointDir = currentPatrolPoint.position - transform.position;
+        float angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x)*Mathf.Rad2Deg -90f;
+
+        Quaternion q = Quaternion.AngleAxis (angle, Vector2.right);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
+    }
+}
---
+++ AxisEnemy/Assets/_Scripts/PatrolRaycastScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolRaycastScript : MonoBehaviour
+{
+    public float speed;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.left, 0.1f);
+
+        if(groundInfo.collider == true)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+                StartCoroutine(microMovementCooldown());
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+                StartCoroutine(microMovementCooldown());
+            }
+        }
+    }
+
+        IEnumerator microMovementCooldown()
+        {
+        Debug.Log("Working");
+
+        /*while (cooldown > 0f)
+        {
+            cooldown -= Time.deltaTime;
+            yield return null;
+        }*/
+        yield return new WaitForSeconds(2);
+
+        yield return null;
+        }
+ 
+}
